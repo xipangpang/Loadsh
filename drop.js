d9cffbb812afe8d8055294ae47891d0d046a89d5
xiyuanyuan01 <xiyuanyuan01@baidu.com>
2022-05-05 20:55:14 +0800

    切片数组

---
+++ drop.js
@@ -0,0 +1,18 @@
+/**
+ * 创建一个切片数组，去除array前面的n个元素。（n默认值为1。）
+ */
+function _drop(array,num) {
+    if(num > array.length){
+        return [];
+    } 
+    const newArr = [];
+    array.forEach((ele,index) => {
+        if(num <index+1) {
+            newArr.push(ele);
+        }
+    });
+    return newArr;
+}
+const arr = [1, 2, 3];
+const num = -1;
+_drop(arr, num)
