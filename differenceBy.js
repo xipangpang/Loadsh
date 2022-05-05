8597ffc2f9daf09228e8a8aaf8ec90e079a48443
xiyuanyuan01 <xiyuanyuan01@baidu.com>
2022-05-05 20:47:07 +0800

    待完成

---
+++ differenceBy.js
@@ -0,0 +1,13 @@
+function _differenceBy(array,value,iteratee) {
+    console.log('+++++++')
+}
+const array = [3.1, 2.2, 1.3];
+const value = [4.4, 2.5];
+const iteratee = Math.floor;
+_differenceBy(array, value, iteratee);
+// console.log(instanceof)
+
+// Array|Function|Object|string
+function _iteratee(item) {
+    // if(item)
+}
