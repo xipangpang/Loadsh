2cbbb507667c77dbf5d22980acbb98e39a72e0d0
xiyuanyuan01 <xiyuanyuan01@baidu.com>
2022-05-09 18:48:08 +0800

    无脑数组扁平化

--- flattenDeep.js
+++ flattenDeep.js
@@ -1,9 +1,9 @@
-/**减少一级array嵌套深度。数组扁平化 */
-function _flatten(array) {
+/**减少一级array嵌套深度；无脑数组扁平化 */
+function _flattenDeep(array) {
     let result = [];
     array.forEach(ele => {
         if (Array.isArray(ele)) {
-            result = result.concat(_flatten(ele));
+            result = result.concat(_flattenDeep(ele));
         } else {
             result.push(ele);
         }
@@ -11,4 +11,4 @@ function _flatten(array) {
     return result;
 };
 const array = [1, [2, [3, [4]], 5]]; 
-console.log(_flatten(array))
+console.log(_flattenDeep(array))
