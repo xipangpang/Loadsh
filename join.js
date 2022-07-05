dcdada17de15cafcddbec34db3d6adf153ffe854
xiyuanyuan01 <xiyuanyuan01@baidu.com>
2022-07-05 17:45:55 +0800

    join方法初实现

---
+++ join.js
@@ -0,0 +1,22 @@
+function join(array, sep) {
+    if (array.length === 0) return '[]';
+    let str = '';
+    for (let i = 0; i < array.length; i++) {
+            // 判断是不是Object
+           if (typeof array[i] === 'object') {
+               if (i === 0) {
+                    str = str + JSON.stringify(array[i]);
+               } else {
+                    str = str + sep + JSON.stringify(array[i]);
+               }
+           }else {
+                if(i === 0) {
+                    str = str + array[i];
+                } else {
+                    str = str + sep + array[i];
+                }
+               
+           }
+        }
+    return str;
+}
