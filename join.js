991b31f60095ca504d8b513f09bf9268df37ec02
xiyuanyuan01 <xiyuanyuan01@baidu.com>
2022-07-05 18:02:27 +0800

    简化逻辑

--- join.js
+++ join.js
@@ -5,16 +5,16 @@ function join (array, sep) {
             // 判断是不是Object
            if (typeof array[i] === 'object') {
                if (i === 0) {
-                    str = str + JSON.stringify(array[i]);
+                    str = JSON.stringify(array[0]);
                } else {
                     str = str + sep + JSON.stringify(array[i]);
                }
             // 避免undefined
-           } else if(typeof array[i] === 'undefined') {
-                if(i !== 0) str = str + sep;
-           }else {
-                if(i === 0) {
-                    str = str + array[i];
+           } else if (typeof array[i] === 'undefined') {
+                if (i !== 0) str = str + sep;
+           } else {
+                if (i === 0) {
+                    str = array[0];
                 } else {
                     str = str + sep + array[i];
                 }
